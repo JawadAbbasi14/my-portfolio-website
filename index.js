09df3b2728acf08a9710429cc7df78975a3174e0
MJ_Abbasi <jawadabbasi1107@gmail.com>
2025-05-11 05:07:06 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function toggleMode() {
+    document.body.classList.toggle('dark');
+    document.body.classList.toggle('light');
+}
+
+// Default to light mode
+document.body.classList.add('light');
